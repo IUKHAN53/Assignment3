a8875beed760de37f0fb352f3f22901b875b3991
IUN KHAN <iu.khan53@outlook.com>
2019-05-30 15:08:56 +0500

    Completed and Added Add

--- delete.xaml.cs
+++ delete.xaml.cs
@@ -21,7 +21,52 @@ namespace Assignment3
     {
         public delete()
         {
+
             InitializeComponent();
+            NORTHWNDEntities1 nw = new NORTHWNDEntities1();
+
+            var res = from pd in nw.Products
+                      select new
+                      {
+                          pd.ProductName
+                      };
+            foreach (var item in res)
+            {
+                productC.Items.Add(item.ProductName.ToString());
+            }
+
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            string item = productC.SelectedItem.ToString();
+            NORTHWNDEntities1 nw = new NORTHWNDEntities1();
+            var order = from x in nw.Order_Details
+                        join v in nw.Products on x.ProductID equals v.ProductID
+                        where v.ProductName==item
+                        select x;
+            foreach (Order_Detail res in order)
+            {
+                nw.Order_Details.Remove(res);
+                
+            }
+            nw.SaveChanges();
+
+
+            var query = from x in nw.Products
+                        where x.ProductName == item
+                        select x;
+            Product result = query.SingleOrDefault();
+            if (result != null)
+            {
+                nw.Products.Remove(result);
+                nw.SaveChanges();
+                confirmLbl.Content = "Product "+item+" Deleted from Database";
+            }
+            else
+            {
+                MessageBox.Show("An Error Occured");
+            }
         }
     }
 }
